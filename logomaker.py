276594a4564950a2d06a2d58da783dea1fe5ffaf
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-21 20:45:25 +0530

    Update logomaker.py

--- logomaker.py
+++ logomaker.py
@@ -1,1 +1,27 @@
+from PIL import Image, ImageFont, ImageDraw
 
+import random
+
+text = "MESSAGE"
+
+photo_path = "resources/logos"
+
+color= "Blue"
+
+X = "resources/fonts"
+
+pp = "photo_path"
+
+fnt = random.choice(os.listdir(X))
+
+image = Image.open(photo)
+
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.truetype(f"{X}/{fnt}", 130)
+
+draw.text((220, 250), text, fill =color, font =font, align ="center")
+
+image.save('output.jpg')
+
+await e.reply(file="output.jpg")
